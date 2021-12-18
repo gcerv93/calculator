105733055bcefc2e3102fae4a409e945fc0b8205
gcerv93 <gcerv93@protonmail.com>
2021-12-18 12:58:37 -0600

    set up file structure

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+function add(n1, n2) {
+  return n1 + n2
+}
+
+function subtract(n1, n2) {
+  if (n1 > n2) return n1 - n2
+  if (n2 > n1) return n2 - n1
+}
+
+function multiply(n1, n2) {
+  return n1 * n2
+}
+
+function divide(n1, n2) {
+  return n1 / n2
+}
