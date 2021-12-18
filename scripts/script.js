a220265f2dbe1b3c81e8984e248b2159e1e38a3b
gcerv93 <gcerv93@protonmail.com>
2021-12-18 13:04:50 -0600

    finish operate function

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,16 @@ function multiply(n1, n2) {
 function divide(n1, n2) {
   return n1 / n2
 }
+
+function operate(op, n1, n2) {
+  switch(op) {
+    case '+':
+      return add(n1, n2);
+    case '-':
+      return subtract(n1, n2);
+    case '*':
+      return multiply(n1, n2);
+    case '/':
+      return divide(n1, n2);
+  }
+}
