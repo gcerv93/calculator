61d51b89e19cd11d3dd8f8ee9e8cebbea91e24b9
gcerv93 <gcerv93@protonmail.com>
2021-12-19 06:57:23 -0600

    working number display

--- scripts/script.js
+++ scripts/script.js
@@ -27,3 +27,22 @@ function operate(op, n1, n2) {
       return divide(n1, n2);
   }
 }
+
+let displayValue = ''
+const items = document.querySelectorAll('.item');
+const display = document.querySelector('.display');
+
+items.forEach((item) =>  {
+  item.addEventListener('click', (e) => {
+    // convert the id to a number, then check if it is in fact a number, if it is a number, populateDisplay
+    if (Number.isInteger(parseInt(e.target.id))) {
+      populateDisplay(e);
+    }
+  })
+})
+
+// update display with target.id, and update the displayValue variable
+function populateDisplay(e) {
+  display.textContent += e.target.id;
+  displayValue = display.textContent;
+}
