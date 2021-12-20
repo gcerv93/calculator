4957d980987e207bb87f5d94e26f7b2aa8364aac
gcerv93 <gcerv93@protonmail.com>
2021-12-20 10:27:27 -0600

    working signChange function

--- scripts/script.js
+++ scripts/script.js
@@ -32,17 +32,37 @@ let displayValue = ''
 const items = document.querySelectorAll('.item');
 const display = document.querySelector('.display');
 
-items.forEach((item) =>  {
-  item.addEventListener('click', (e) => {
-    // convert the id to a number, then check if it is in fact a number, if it is a number, populateDisplay
-    if (Number.isInteger(parseInt(e.target.id))) {
-      populateDisplay(e);
-    }
-  })
-})
+// button event listener
+items.forEach((item) => item.addEventListener('click', handleButtons))
 
 // update display with target.id, and update the displayValue variable
 function populateDisplay(e) {
+  // do nothing if a '.' already exists in display
+  if (e.target.id == '.') {
+    if (display.textContent.includes('.')) {
+      return
+    }
+  }
   display.textContent += e.target.id;
+  displayValue = display.textContent;
+}
+
+function handleButtons(e) {
+  // convert the target id to a number, then check if it is in fact a number, if it is a number, populateDisplay
+  if (Number.isInteger(parseInt(e.target.id))) {
+    populateDisplay(e);
+  } else if (e.target.id == ".") {
+    populateDisplay(e);
+  } else if (e.target.id == 'neg') {
+    signChange(e);
+  }
+}
+
+// checks if a negative sign already exists in the display, if it does, remove it, if it doesnt, add it to the begining of said display
+function signChange() {
+  let sign = '-'
+  display.textContent.includes('-') ? display.textContent = displayValue.substring(1) : display.textContent = sign += display.textContent;
+
+  // reassign the displayValue variable
   displayValue = display.textContent;
 }
